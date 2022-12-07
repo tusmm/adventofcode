ce21f6c2bb5b1ee36c4835cd8b4522dbb65719e8
tusmm <r.ong40915@gmail.com>
2022-12-06 20:05:15 -0500

    day1 part2

--- day1/day1.py
+++ day1/day1.py
@@ -2,16 +2,21 @@
 total each line then compare to the calorie counter
 """
 
-mostCalories = 0;
+calories = []
 
 with open("input1.txt", 'r') as f:
     elfCalorie = 0
     for line in f:
         if line == '\n':
-            if mostCalories < elfCalorie:
-                mostCalories = elfCalorie
+            calories.append(elfCalorie)
             elfCalorie = 0
         else:
             elfCalorie += int(line.strip())
-        
-print(mostCalories)
+
+top3Cal = sorted(calories)[-3:]
+
+total = 0
+for cal in top3Cal:
+    total += cal
+
+print(total)
