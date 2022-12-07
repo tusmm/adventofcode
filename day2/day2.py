c01ca7d9b9526b31feb89ae7eacafa19ea15517d
tusmm <r.ong40915@gmail.com>
2022-12-06 20:42:58 -0500

    day2 part 1 finished

---
+++ day2/day2.py
@@ -0,0 +1,18 @@
+scores = {
+    'A X': 3 + 1,
+    'A Y': 6 + 2,
+    'A Z': 0 + 3,
+    'B X': 0 + 1,
+    'B Y': 3 + 2,
+    'B Z': 6 + 3,
+    'C X': 6 + 1,
+    'C Y': 0 + 2,
+    'C Z': 3 + 3,
+}
+
+score = 0
+with open("input.txt", "r") as f:
+    for line in f:
+        score += scores[line.strip()]
+
+print(score)
