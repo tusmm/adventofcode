0d6326999e01c7fed56e7a986706ffe5de6723cc
tusmm <r.ong40915@gmail.com>
2022-12-06 21:34:09 -0500

    day2 part2 fin

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,4 @@
+# possible combinations
 scores = {
     'A X': 3 + 1,
     'A Y': 6 + 2,
@@ -10,9 +11,40 @@ scores = {
     'C Z': 3 + 3,
 }
 
+# options
+op = {
+    'A': 1,
+    'B': 2,
+    'C': 3
+}
+op2 = {
+    0: 'A',
+    1: 'B',
+    2: 'C'
+}
+
+# loss
+# 1, 2
+# 2, 0
+# 3, 1
+
+# win
+# 1, 1
+# 2, 2
+# 3, 0
+
 score = 0
 with open("input.txt", "r") as f:
     for line in f:
-        score += scores[line.strip()]
-
+        strat = line[2]
+        if strat == 'X':
+            # lose
+            score += op[op2[(op[line[0]] + 1) % 3]]
+        elif strat == 'Y':
+            # draw
+            score += (op[line[0]]) + 3
+        elif strat == 'Z':
+            # win
+            score += (op[op2[(op[line[0]]) % 3]]) + 6
+            
 print(score)
